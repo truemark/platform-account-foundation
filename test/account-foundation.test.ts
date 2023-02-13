faf7ef91298af1a472a6d251d67e8f72b3e60622
Erik Jensen <ejensen@truemark.io>
2023-02-13 07:43:15 -0700

    Initial code

---
+++ test/account-foundation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AccountFoundation from '../lib/account-foundation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/account-foundation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AccountFoundation.AccountFoundationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
